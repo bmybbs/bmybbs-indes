44d9658a75c9c84a6db05a6798e6e7cfd147d8ec
IronBlood <aplshenyang@gmail.com>
2021-04-23 13:22:49 +0800

    将 article timestamp 纳入索引

--- java/src/edu/xjtu/bmybbs/Constants.java
+++ java/src/edu/xjtu/bmybbs/Constants.java
@@ -8,5 +8,6 @@ public class Constants {
 	public static final String FIELD_FILENAME = "name";
 	public static final String FIELD_OWNER = "owner";
 	public static final String FIELD_TITLE = "title";
+	public static final String FIELD_TIMESTAMP = "timestamp";
 	public static final String FIELD_CONTENTS = "contents";
 }
